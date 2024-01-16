2db0ac7e14d0acbae0aec42e98323383966c7bb5
Joshua Immordino <joshimmor@gmail.com>
2024-01-16 11:56:30 -0500

    refresh

--- app/player/[id]/page.js
+++ app/player/[id]/page.js
@@ -1,20 +1,38 @@
-import React from 'react'
-
-
+"use client"
+import React, { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
+import TvConfig from "../../tv.json"
 export default function Page({params}) {
+  let tvChannelID = params.id;
+  let tvChannel = TvConfig['channels'].filter(n => n.id == tvChannelID)[0]
+  let [mutedoption,setmuted] = useState(true)
+  let [playingoption,setplaying] = useState(false)
+  useEffect(()=>{
+    if((ReactPlayer.canPlay(tvChannel.stream))){
+      setTimeout(() => {
+        setmuted(false)
+        setplaying(true)
+      }, 3500)
+    }else{
+      window.location.reload()
+    }
 
-  let tvChannel = params.id;
+  },[tvChannel])
+  function refresh(){
+    window.location.reload()
+  }
   return (
-    <div className='w-screen h-screen'>
-      <iframe className="video responsive" 
-      marginHeight="0" marginWidth="0" src={`https://weblivehdplay.ru/premiumtv/daddylivehd.php?id=${tvChannel}`} 
-      name="iframe_a" allow="autoplay" allowFullScreen="yes" width="100%" 
-      height="100%" >
-        Your Browser Do not Support Iframe</iframe>
-    </div>
+  <div className='w-screen h-screen'>
+    <button onClick={refresh} className="z-10 absolute rounded-md bg-indigo-600 m-2 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Refresh</button>
+    <ReactPlayer 
+    className="" 
+    url={tvChannel.stream} 
+    controls
+    playing={playingoption}
+    muted={mutedoption}
+    width='100vw'
+    height='100%'/>
+  </div>
+
   )
 }
-// fox 347
-//SNY 759
-// ESPN 44
-// TruTv 341
--- components/ProductPlacement.js
+++ components/ProductPlacement.js
@@ -1,36 +1,41 @@
 "use client"
 import { useEffect,useState } from "react"
 import Products from "./Products"
-
+import TvConfig from "../app/tv.json"
 
 export default  function ProductPlacement() {
-  let channels = [
-    {
-        channelName:"Fox",
-        logoPath:"/Fox.jpg",
-        id:347
-    },
-    {
-        channelName:"SNY",
-        logoPath:"/SNY.jpg",
-        id:759
-    },
-    {
-        channelName:"ESPN",
-        logoPath:"/ESPN.webp",
-        id:44
-    },
-    {
-        channelName:"TruTv",
-        logoPath:"/TruTv.webp",
-        id:341
-    }
-  ]
+  let channels = TvConfig['channels']
+  // let channels = [
+  //   {
+  //       channelName:"Fox",
+  //       logoPath:"/Fox.jpg",
+  //       id:347,
+  //       stream:"http://247preview.foxnews.com/hls/live/2020027/fncv3preview/primary.m3u8"
+  //   },
+  //   {
+  //       channelName:"SNY",
+  //       logoPath:"/SNY.jpg",
+  //       id:759,
+  //       stream:""
+  //   },
+  //   {
+  //       channelName:"ESPN",
+  //       logoPath:"/ESPN.webp",
+  //       id:44,
+  //       stream:""
+  //   },
+  //   {
+  //       channelName:"TruTv",
+  //       logoPath:"/TruTv.webp",
+  //       id:341,
+  //       stream:""
+  //   }
+  // ]
 
   return (
     <div className="grid grid-cols-4 gap-2 p-2 justify justify-items-center  items-center h-screen">
       {channels.map((n,i)=>{
-          return <Products key={i} id={n.id} logo={n.logoPath} name={n.channelName}/>
+          return <Products key={i} id={n.id} logo={n.logoPath} name={n.channelName} stream={n.stream}/>
         })}
     </div>
   )
