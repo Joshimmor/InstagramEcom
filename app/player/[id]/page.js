82bd1f2dc20e8dd129ff1be5e21ecfb92fed5e15
Joshua Immordino <joshimmor@gmail.com>
2024-01-16 22:21:54 -0500

    polishing

--- app/player/[id]/page.js
+++ app/player/[id]/page.js
@@ -2,38 +2,34 @@
 import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 import TvConfig from "../../tv.json"
+import Link from "next/link"
 export default function Page({params}) {
   let tvChannelID = params.id;
   let tvChannel = TvConfig['channels'].filter(n => n.id == tvChannelID)[0]
   let [mutedoption,setmuted] = useState(true)
   let [playingoption,setplaying] = useState(false)
-  // useEffect(()=>{
-  //   if(ReactPlayer.canPlay(tvChannel.stream)){
-  //     setTimeout(() => {
-  //       setmuted(false)
-  //     }, 3500)
-  //   }else{
-  //     window.location.reload()
-  //   }
 
-  // },[tvChannel])
-  function refresh(){
-    window.location.reload()
+  function ready(){
+    setmuted(false); 
+    setplaying(true)
   }
+  
   return (
-  <div className='w-screen h-screen'>
-    <button onClick={refresh} className="z-10 absolute rounded-md bg-indigo-600 m-2 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Refresh</button>
+  <div className='w-screen h-screen bg-gradient-to-b from-neutral-900 to-slate-800'>
+    <Link href="/" className="z-10 absolute m-4 flex w-20 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+      </svg>
+    </Link>
     <ReactPlayer 
-
     className="" 
     url={tvChannel.stream} 
     controls
-
     playing={playingoption}
-    onReady={()=>{setmuted(false); setplaying(true)}}
+    onReady={ready}
     muted={mutedoption}
-    width='100vw'
-    height='100%'/>
+    height="100%"
+    width="100vw"/>
   </div>
 
   )
--- components/ProductPlacement.js
+++ components/ProductPlacement.js
@@ -5,35 +5,8 @@ import TvConfig from "../app/tv.json"
 
 export default  function ProductPlacement() {
   let channels = TvConfig['channels']
-  // let channels = [
-  //   {
-  //       channelName:"Fox",
-  //       logoPath:"/Fox.jpg",
-  //       id:347,
-  //       stream:"http://247preview.foxnews.com/hls/live/2020027/fncv3preview/primary.m3u8"
-  //   },
-  //   {
-  //       channelName:"SNY",
-  //       logoPath:"/SNY.jpg",
-  //       id:759,
-  //       stream:""
-  //   },
-  //   {
-  //       channelName:"ESPN",
-  //       logoPath:"/ESPN.webp",
-  //       id:44,
-  //       stream:""
-  //   },
-  //   {
-  //       channelName:"TruTv",
-  //       logoPath:"/TruTv.webp",
-  //       id:341,
-  //       stream:""
-  //   }
-  // ]
-
   return (
-    <div className="grid grid-cols-4 gap-2 p-2 justify justify-items-center  items-center h-screen">
+    <div className="grid grid-cols-4 gap-2 p-2 justify justify-items-center  items-center h-screen bg-gradient-to-b from-neutral-100 to-slate-200">
       {channels.map((n,i)=>{
           return <Products key={i} id={n.id} logo={n.logoPath} name={n.channelName} stream={n.stream}/>
         })}
